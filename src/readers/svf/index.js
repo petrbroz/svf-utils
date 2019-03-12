705b1af20d0ca048428e5136b5b7fe2961453072
Petr Broz <petr.broz@autodesk.com>
2019-03-12 20:37:02 +0000

    Moved deriv. service helpers into a separate script.

--- src/readers/svf/index.js
+++ src/readers/svf/index.js
@@ -1,43 +1,10 @@
-const fs = require('fs');
-const path = require('path');
-const fetch = require('node-fetch');
 const Zip = require('node-zip');
 
+const { getManifest, getDerivative, traverseManifest } = require('../../helpers/forge');
 const { FragmentReader } = require('./fragment-reader');
 const { GeometryReader } = require('./geometry-reader');
 const { MeshReader } = require('./mesh-reader');
 const { MaterialReader } = require('./material-reader');
-
-const BaseUrl = 'https://developer.api.autodesk.com';
-
-async function getManifest(urn, token) {
-    const res = await fetch(`${BaseUrl}/modelderivative/v2/designdata/${urn}/manifest`, {
-        compress: true,
-        headers: { 'Authorization': 'Bearer ' + token }
-    });
-    return res.json();
-}
-
-async function getDerivative(urn, token) {
-    const res = await fetch(`${BaseUrl}/derivativeservice/v2/derivatives/${urn}`, {
-        compress: true,
-        headers: { 'Authorization': 'Bearer ' + token }
-    });
-    return res.buffer();
-}
-
-function traverseManifest(node, callback) {
-    callback(node);
-    if (node.derivatives) {
-        for (const child of node.derivatives) {
-            traverseManifest(child, callback);
-        }
-    } else if (node.children) {
-        for (const child of node.children) {
-            traverseManifest(child, callback);
-        }
-    }
-}
 
 async function getViewable(urn, token, guid) {
     const manifest = await getManifest(urn, token);

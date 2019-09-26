107c98dc92b4b439c0a28808a8483c03104addf4
Petr Broz <ipetrbroz@gmail.com>
2019-09-26 12:19:59 -0400

    Reusing SVF parser from forge-server-utils module.

--- src/svf.ts
+++ src/svf.ts
@@ -1,96 +1,41 @@
-import { ModelDerivativeClient, ManifestHelper, IDerivativeResourceChild } from 'forge-server-utils';
 import { IAuthOptions } from 'forge-server-utils/dist/common';
-import * as svf from 'forge-server-utils/dist/svf';
+import * as SvfUtils from 'forge-server-utils/dist/svf';
 
 export interface ISvf {
-    metadata: svf.ISvfMetadata;
-    fragments: svf.IFragment[];
-    geometries: svf.IGeometryMetadata[];
-    meshpacks: (svf.IMesh | null)[][];
-    materials: (svf.IMaterial | null)[];
+    metadata: SvfUtils.ISvfMetadata;
+    fragments: SvfUtils.IFragment[];
+    geometries: SvfUtils.IGeometryMetadata[];
+    meshpacks: (SvfUtils.IMesh | null)[][];
+    materials: (SvfUtils.IMaterial | null)[];
     getDerivative: (uri: string) => Promise<Buffer>;
 }
 
 export async function deserialize(urn: string, guid: string, auth: IAuthOptions): Promise<ISvf> {
-    const modelDerivativeClient = new ModelDerivativeClient(auth);
-    const helper = new ManifestHelper(await modelDerivativeClient.getManifest(urn));
-    const svfDerivatives = helper.search({ type: 'resource', role: 'graphics', guid });
-    if (svfDerivatives.length === 0) {
-        throw new Error(`Guid ${guid} not found in urn ${urn}.`);
-    }
-    const svfDerivative = svfDerivatives[0] as IDerivativeResourceChild;
-    const buffer = await modelDerivativeClient.getDerivative(urn, svfDerivative.urn);
-    const { manifest, metadata } = svf.parseManifest(buffer as Buffer);
-    const baseUri = svfDerivative.urn.substr(0, svfDerivative.urn.lastIndexOf('/'));
+    const deserializer = await SvfUtils.Parser.FromDerivativeService(urn, guid, auth);
     let output: ISvf = {
-        metadata,
+        metadata: await deserializer.getMetadata(),
         fragments: [],
         geometries: [],
         meshpacks: [],
         materials: [],
-        getDerivative: async (uri: string) => {
-            const buffer = await modelDerivativeClient.getDerivative(urn, baseUri + '/' + uri) as Buffer;
-            return buffer;
-        }
+        getDerivative: (uri: string) => deserializer.getAsset(uri)
     };
     let tasks: Promise<void>[] = [];
-    for (const asset of manifest.assets) {
-        if (asset.URI.startsWith('embed:')) {
-            continue; // For now we only expect 'manifest.json' and 'metadata.json' to be embedded
-        }
-        const assetUri = baseUri + '/' + asset.URI;
-        switch (asset.type) {
-            case svf.AssetType.FragmentList:
-                console.log(`Parsing fragments (${assetUri})...`);
-                tasks.push(deserializeFragments(urn, assetUri, output, modelDerivativeClient));
-                break;
-            case svf.AssetType.GeometryMetadataList:
-                console.log(`Parsing geometry metadata (${assetUri})...`);
-                tasks.push(deserializeGeometries(urn, assetUri, output, modelDerivativeClient));
-                break;
-            case svf.AssetType.PackFile:
-                console.log(`Parsing mesh pack (${assetUri})...`);
-                const typeset = manifest.typesets[parseInt(asset.typeset || '')];
-                if (typeset.types[0].class === 'Autodesk.CloudPlatform.Geometry') {
-                    let meshpack: svf.IMesh[] = [];
-                    output.meshpacks.push(meshpack);
-                    tasks.push(deserializeMeshes(urn, assetUri, meshpack, modelDerivativeClient));
-                }
-                break;
-            case svf.AssetType.ProteinMaterials:
-                if (assetUri.indexOf('ProteinMaterials.json.gz') !== -1) {
-                    continue; // Ignore the advanced materials, we only support basic materials (Materials.json.gz)
-                }
-                console.log(`Parsing materials (${assetUri})...`);
-                tasks.push(deserializeMaterials(urn, assetUri, output, modelDerivativeClient));
-                break;
-            default:
-                console.log(`Skipping unsupported asset type: ${asset.type}`);
-                break;
-        }
+
+    tasks.push((async function() {
+        output.fragments = await deserializer.listFragments();
+    })());
+    tasks.push((async function() {
+        output.geometries = await deserializer.listGeometries();
+    })());
+    tasks.push((async function() {
+        output.materials = await deserializer.listMaterials();
+    })());
+    for (let i = 0, len = deserializer.getMeshPackCount(); i < len; i++) {
+        tasks.push((async function(id: number) {
+            output.meshpacks[id] = await deserializer.listMeshPack(id);
+        })(i));
     }
     await Promise.all(tasks);
     return output;
-}
-
-async function deserializeFragments(urn: string, assetUri: string, output: ISvf, client: ModelDerivativeClient) {
-    const buffer = await client.getDerivative(urn, assetUri);
-    output.fragments = Array.from(svf.parseFragments(buffer as Buffer));
-}
-
-async function deserializeGeometries(urn: string, assetUri: string, output: ISvf, client: ModelDerivativeClient) {
-    const buffer = await client.getDerivative(urn, assetUri);
-    output.geometries = Array.from(svf.parseGeometries(buffer as Buffer));
-}
-
-async function deserializeMeshes(urn: string, assetUri: string, meshpack: (svf.IMesh | null)[], client: ModelDerivativeClient) {
-    const buffer = await client.getDerivative(urn, assetUri);
-    for (const mesh of svf.parseMeshes(buffer as Buffer)) {
-        meshpack.push(mesh);
-    }
-}
-
-async function deserializeMaterials(urn: string, assetUri: string, output: ISvf, client: ModelDerivativeClient) {
-    const buffer = await client.getDerivative(urn, assetUri);
-    output.materials = Array.from(svf.parseMaterials(buffer as Buffer));
 }

09e14026f53f250130c892b5a24b1155877d4d16
Petr Broz <ipetrbroz@gmail.com>
2019-10-11 11:47:26 +0200

    Added gltf-pipeline optimization to bash examples.

--- test/local-svf-to-gltf.sh
+++ test/local-svf-to-gltf.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-# Example: converting an SVF (without property database) from local file system to glTF
+# Example: converting an SVF (without property database) from local file system to glTF,
+# and optimizing the result into a GLB format with Draco compression
 # Usage:
 #     npm install --global forge-convert-utils
 #     ./local-svf-to-gltf.sh <path to svf file> <path to output folder>
 
-npm install --global forge-convert-utils
+npm install --global forge-convert-utils gltf-pipeline
 forge-convert $1 --output-folder $2 --deduplicate
+gltf-pipeline -i $2/output.gltf -o $2/output.glb -d
--- test/remote-svf-to-gltf.sh
+++ test/remote-svf-to-gltf.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-# Example: converting an SVF (without property database) from local file system into glTF
+# Example: converting an SVF (without property database) from local file system into glTF,
+# and optimizing the result into a GLB format with Draco compression
 # Usage:
 #     npm install --global forge-convert-utils
 #     export FORGE_CLIENT_ID=<your client id>
 #     export FORGE_CLIENT_SECRET=<your client secret>
 #     ./remote-svf-to-gltf.sh <your model urn> <path to output folder>
 
-npm install --global forge-convert-utils
+npm install --global forge-convert-utils gltf-pipeline
 forge-convert $1 --output-folder $2 --deduplicate
+gltf-pipeline -i $2/output.gltf -o $2/output.glb -d

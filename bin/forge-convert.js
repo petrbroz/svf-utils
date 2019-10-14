12428145494c0c4bbf747c60d0a2e9e3100d806f
Petr Broz <ipetrbroz@gmail.com>
2019-10-14 23:32:22 +0200

    Fixed missing auth object & added logs in CLI tool.

--- bin/forge-convert.js
+++ bin/forge-convert.js
@@ -3,23 +3,36 @@
 const program = require('commander');
 const path = require('path');
 const fse = require('fs-extra');
+const debug = require('debug');
 const { ModelDerivativeClient, ManifestHelper } = require('forge-server-utils');
 
 const { SvfReader, GltfWriter } = require('..');
 
+const { FORGE_CLIENT_ID, FORGE_CLIENT_SECRET, FORGE_ACCESS_TOKEN } = process.env;
+let auth = null;
+if (FORGE_ACCESS_TOKEN) {
+    auth = { token: FORGE_ACCESS_TOKEN };
+} else if (FORGE_CLIENT_ID && FORGE_CLIENT_SECRET) {
+    auth = { client_id: FORGE_CLIENT_ID, client_secret: FORGE_CLIENT_SECRET };
+}
+
 async function convertRemote(urn, guid, outputFolder, options) {
-    console.log('Converting urn', urn, 'guid', guid);
+    const log = debug('cli');
+    log(`Converting urn ${urn}, guid ${guid}`);
     const reader = await SvfReader.FromDerivativeService(urn, guid, auth);
-    const svf = await reader.read();
+    const svf = await reader.read({ log: debug('cli:reader') });
+    options.log = debug('cli:writer');
     const writer = new GltfWriter(outputFolder, options);
     writer.write(svf);
     await writer.close();
 }
 
 async function convertLocal(svfPath, outputFolder, options) {
-    console.log('Converting local file', svfPath);
+    const log = debug('cli');
+    log(`Converting local file ${svfPath}`);
     const reader = await SvfReader.FromFileSystem(svfPath);
-    const svf = await reader.read();
+    const svf = await reader.read({ log: debug('cli:reader') });
+    options.log = debug('cli:writer');
     const writer = new GltfWriter(outputFolder, options);
     writer.write(svf);
     await writer.close();
@@ -46,13 +59,6 @@ program
             } else {
                 // ID is the Model Derivative URN
                 // Convert input guid or all guids
-                const { FORGE_CLIENT_ID, FORGE_CLIENT_SECRET, FORGE_ACCESS_TOKEN } = process.env;
-                let auth = null;
-                if (FORGE_ACCESS_TOKEN) {
-                    auth = { token: FORGE_ACCESS_TOKEN };
-                } else if (FORGE_CLIENT_ID && FORGE_CLIENT_SECRET) {
-                    auth = { client_id: FORGE_CLIENT_ID, client_secret: FORGE_CLIENT_SECRET };
-                }
                 if (!auth) {
                     console.warn('Missing environment variables for Autodesk Forge authentication.');
                     console.warn('Provide FORGE_CLIENT_ID and FORGE_CLIENT_SECRET, or FORGE_ACCESS_TOKEN.');

3c389d7872f020f37b6ca40bca2e754250992685
Petr Broz <ipetrbroz@gmail.com>
2019-10-17 15:53:33 +0200

    Updated sample scripts.

--- test/local-svf-to-gltf.js
+++ test/local-svf-to-gltf.js
@@ -10,29 +10,29 @@ const path = require('path');
 const { SvfReader, GltfWriter } = require('..');
 
 async function run (filepath, outputDir) {
-    const reader = await SvfReader.FromFileSystem(filepath);
-    const svf = await reader.read();
-    let writer;
-    writer = new GltfWriter(path.join(outputDir, 'gltf-nodedup'), { deduplicate: false, binary: false, compress: false, log: console.log });
-    writer.write(svf);
-    await writer.close();
-    writer = new GltfWriter(path.join(outputDir, 'gltf'), { deduplicate: true, binary: false, compress: false, log: console.log });
-    writer.write(svf);
-    await writer.close();
-    writer = new GltfWriter(path.join(outputDir, 'gltf-draco'), { deduplicate: true, binary: false, compress: true, log: console.log });
-    writer.write(svf);
-    await writer.close();
-    writer = new GltfWriter(path.join(outputDir, 'glb'), { deduplicate: true, binary: true, compress: false, log: console.log });
-    writer.write(svf);
-    await writer.close();
-    writer = new GltfWriter(path.join(outputDir, 'glb-draco'), { deduplicate: true, binary: true, compress: true, log: console.log });
-    writer.write(svf);
-    await writer.close();
+    try {
+        const reader = await SvfReader.FromFileSystem(filepath);
+        const svf = await reader.read();
+        let writer;
+        writer = new GltfWriter(path.join(outputDir, 'gltf-nodedup'), { deduplicate: false, binary: false, compress: false, log: console.log });
+        writer.write(svf);
+        await writer.close();
+        writer = new GltfWriter(path.join(outputDir, 'gltf'), { deduplicate: true, binary: false, compress: false, log: console.log });
+        writer.write(svf);
+        await writer.close();
+        writer = new GltfWriter(path.join(outputDir, 'gltf-draco'), { deduplicate: true, binary: false, compress: true, log: console.log });
+        writer.write(svf);
+        await writer.close();
+        writer = new GltfWriter(path.join(outputDir, 'glb'), { deduplicate: true, binary: true, compress: false, log: console.log });
+        writer.write(svf);
+        await writer.close();
+        writer = new GltfWriter(path.join(outputDir, 'glb-draco'), { deduplicate: true, binary: true, compress: true, log: console.log });
+        writer.write(svf);
+        await writer.close();
+    } catch(err) {
+        console.error(err);
+        process.exit(1);
+    }
 }
 
-try {
-    run(process.argv[2], process.argv[3]);
-} catch(err) {
-    console.error(err);
-    process.exit(1);
-}
+run(process.argv[2], process.argv[3]);
--- test/remote-svf-to-gltf.js
+++ test/remote-svf-to-gltf.js
@@ -8,27 +8,31 @@
  */
 
 const path = require('path');
-const debug = require('debug');
 const { ModelDerivativeClient, ManifestHelper } = require('forge-server-utils');
 const { SvfReader, GltfWriter } = require('..');
 
 const { FORGE_CLIENT_ID, FORGE_CLIENT_SECRET } = process.env;
 
 async function run (urn, outputDir) {
-    const auth = { client_id: FORGE_CLIENT_ID, client_secret: FORGE_CLIENT_SECRET };
-    const modelDerivativeClient = new ModelDerivativeClient(auth);
-    const helper = new ManifestHelper(await modelDerivativeClient.getManifest(urn));
-    const derivatives = helper.search({ type: 'resource', role: 'graphics' });
-    const writer1 = new GltfWriter(path.join(outputDir, 'gltf'), { deduplicate: true, binary: false, compress: false, log: debug('writer:1') });
-    const writer2 = new GltfWriter(path.join(outputDir, '/glb-draco'), { deduplicate: true, binary: true, compress: true, log: debug('writer:2') });
-    for (const derivative of derivatives.filter(d => d.mime === 'application/autodesk-svf')) {
-        const reader = await SvfReader.FromDerivativeService(urn, derivative.guid, auth);
-        const svf = await reader.read({ log: debug('reader') });
-        writer1.write(svf);
-        writer2.write(svf);
+    try {
+        const auth = { client_id: FORGE_CLIENT_ID, client_secret: FORGE_CLIENT_SECRET };
+        const modelDerivativeClient = new ModelDerivativeClient(auth);
+        const helper = new ManifestHelper(await modelDerivativeClient.getManifest(urn));
+        const derivatives = helper.search({ type: 'resource', role: 'graphics' });
+        const writer1 = new GltfWriter(path.join(outputDir, 'gltf'), { deduplicate: true, binary: false, compress: false, log: console.log });
+        const writer2 = new GltfWriter(path.join(outputDir, 'glb-draco'), { deduplicate: true, binary: true, compress: true, log: console.log });
+        for (const derivative of derivatives.filter(d => d.mime === 'application/autodesk-svf')) {
+            const reader = await SvfReader.FromDerivativeService(urn, derivative.guid, auth);
+            const svf = await reader.read({ log: console.log });
+            writer1.write(svf);
+            writer2.write(svf);
+        }
+        await writer1.close();
+        await writer2.close();
+    } catch(err) {
+        console.error(err);
+        process.exit(1);
     }
-    await writer1.close();
-    await writer2.close();
 }
 
 run(process.argv[2], process.argv[3]);

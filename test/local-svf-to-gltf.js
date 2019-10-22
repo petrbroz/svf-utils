cd622fc1cfaa76b17e8ab256615501ed552dd53a
Petr Broz <ipetrbroz@gmail.com>
2019-10-22 15:18:47 +0100

    Updated test files.

--- test/local-svf-to-gltf.js
+++ test/local-svf-to-gltf.js
@@ -9,23 +9,31 @@ const path = require('path');
 const { SvfReader, GltfWriter } = require('..');
 
 async function run (filepath, outputDir) {
+    const defaultOptions = {
+        deduplicate: false,
+        skipUnusedUvs: false,
+        binary: false,
+        compress: false,
+        log: console.log
+    };
+
     try {
         const reader = await SvfReader.FromFileSystem(filepath);
         const svf = await reader.read();
         let writer;
-        writer = new GltfWriter(path.join(outputDir, 'gltf-nodedup'), { deduplicate: false, skipUnusedUvs: false, binary: false, compress: false, log: console.log });
+        writer = new GltfWriter(path.join(outputDir, 'gltf-raw'), Object.assign({}, defaultOptions));
         writer.write(svf);
         await writer.close();
-        writer = new GltfWriter(path.join(outputDir, 'gltf'), { deduplicate: true, skipUnusedUvs: true, binary: false, compress: false, log: console.log });
+        writer = new GltfWriter(path.join(outputDir, 'gltf'), Object.assign({}, defaultOptions, { deduplicate: true, skipUnusedUvs: true }));
         writer.write(svf);
         await writer.close();
-        writer = new GltfWriter(path.join(outputDir, 'gltf-draco'), { deduplicate: true, skipUnusedUvs: true, binary: false, compress: true, log: console.log });
+        writer = new GltfWriter(path.join(outputDir, 'gltf-draco'), Object.assign({}, defaultOptions, { deduplicate: true, skipUnusedUvs: true, compress: true }));
         writer.write(svf);
         await writer.close();
-        writer = new GltfWriter(path.join(outputDir, 'glb'), { deduplicate: true, skipUnusedUvs: true, binary: true, compress: false, log: console.log });
+        writer = new GltfWriter(path.join(outputDir, 'glb'), Object.assign({}, defaultOptions, { deduplicate: true, skipUnusedUvs: true, binary: true }));
         writer.write(svf);
         await writer.close();
-        writer = new GltfWriter(path.join(outputDir, 'glb-draco'), { deduplicate: true, skipUnusedUvs: true, binary: true, compress: true, log: console.log });
+        writer = new GltfWriter(path.join(outputDir, 'glb-draco'), Object.assign({}, defaultOptions, { deduplicate: true, skipUnusedUvs: true, binary: true, compress: true }));
         writer.write(svf);
         await writer.close();
     } catch(err) {
--- test/remote-svf-to-gltf.js
+++ test/remote-svf-to-gltf.js
@@ -14,14 +14,22 @@ const { SvfReader, GltfWriter } = require('..');
 const { FORGE_CLIENT_ID, FORGE_CLIENT_SECRET } = process.env;
 
 async function run (urn, outputDir) {
+    const defaultOptions = {
+        deduplicate: false,
+        skipUnusedUvs: false,
+        binary: false,
+        compress: false,
+        log: console.log
+    };
+
     try {
         const auth = { client_id: FORGE_CLIENT_ID, client_secret: FORGE_CLIENT_SECRET };
         const modelDerivativeClient = new ModelDerivativeClient(auth);
         const helper = new ManifestHelper(await modelDerivativeClient.getManifest(urn));
         const derivatives = helper.search({ type: 'resource', role: 'graphics' });
-        const writer0 = new GltfWriter(path.join(outputDir, 'gltf-nodedup'), { deduplicate: false, skipUnusedUvs: false, binary: false, compress: false, log: console.log });
-        const writer1 = new GltfWriter(path.join(outputDir, 'gltf'), { deduplicate: true, skipUnusedUvs: true, binary: false, compress: false, log: console.log });
-        const writer2 = new GltfWriter(path.join(outputDir, 'glb-draco'), { deduplicate: true, skipUnusedUvs: true, binary: true, compress: true, log: console.log });
+        const writer0 = new GltfWriter(path.join(outputDir, 'gltf-raw'), Object.assign({}, defaultOptions));
+        const writer1 = new GltfWriter(path.join(outputDir, 'gltf'), Object.assign({}, defaultOptions, { deduplicate: true, skipUnusedUvs: true }));
+        const writer2 = new GltfWriter(path.join(outputDir, 'glb-draco'), Object.assign({}, defaultOptions, { deduplicate: true, skipUnusedUvs: true, binary: true, compress: true }));
         for (const derivative of derivatives.filter(d => d.mime === 'application/autodesk-svf')) {
             const reader = await SvfReader.FromDerivativeService(urn, derivative.guid, auth);
             const svf = await reader.read({ log: console.log });

c1410b259678b986b8d6368109e59f75908a0917
Petr Broz <ipetrbroz@gmail.com>
2019-10-29 16:06:45 +0100

    One more update to the sqlite-to-gltf sample.

--- test/extract-sqlite-subset.js
+++ test/extract-sqlite-subset.js
@@ -1,9 +1,14 @@
+// Script for generating new glTF from a sqlite manifest using a specific filter,
+// in this case only including objects with "Structural Material" property containing the word "Concrete".
+// Usage:
+//     node test/extract-sqlite-subset.js path/to/manifest.sqlite path/to/new.gltf
+
 const fse = require('fs-extra');
 
 const { deserialize } = require('../lib/gltf/sqlite');
 
 async function extract(inputSqlitePath, newGltfPath) {
-    const filter = 'SELECT DISTINCT dbid FROM properties WHERE name = "Material" AND value = "Grass"';
+    const filter = 'SELECT DISTINCT dbid FROM properties WHERE name = "Structural Material" AND value LIKE "%Concrete%"';
     console.log('Creating new gltf:', newGltfPath);
     console.log('From sqlite manifest:', inputSqlitePath);
     console.log('Only including objects matching:', filter);

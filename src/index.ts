84eee748fb647c8810bc2fbcd5c2f9e5cb877b2d
Petr Broz <ipetrbroz@gmail.com>
2019-12-20 12:01:48 +0100

    Updated resolving of OTG urns and folder structure.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { Reader as SvfReader } from './svf/reader';
 export { Writer as GltfWriter } from './gltf/writer';
-export { Client as OtgClient } from './otg/client';
+export { Client as OtgClient, ManifestHelper as OtgManifestHelper, ViewHelper as OtgViewHelper } from './otg/client';

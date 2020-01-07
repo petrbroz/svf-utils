084a051b647c10b730ef0c83b7c87aa8463272fd
Petr Broz <ipetrbroz@gmail.com>
2020-01-07 16:10:14 +0100

    Removed experimental OTG code.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export { Reader as SvfReader } from './svf/reader';
 export { Writer as GltfWriter } from './gltf/writer';
 export { Reader as OtgReader } from './otg/reader';
-export { Client as OtgClient, ManifestHelper as OtgManifestHelper, ViewHelper as OtgViewHelper } from './otg/client';

771ff243925d073ba78c40f7f8890309ca11ea56
Petr Broz <ipetrbroz@gmail.com>
2020-02-26 16:04:17 +0100

    Fixed typo in one of the test files.

--- test/remote-svf-to-gltf.js
+++ test/remote-svf-to-gltf.js
@@ -1,5 +1,5 @@
 /*
- * Example: converting an SVF (without property database) from Model Derivative service.
+ * Example: converting an SVF (incl. property database) from Model Derivative service.
  * Usage:
  *     export FORGE_CLIENT_ID=<your client id>
  *     export FORGE_CLIENT_SECRET=<your client secret>

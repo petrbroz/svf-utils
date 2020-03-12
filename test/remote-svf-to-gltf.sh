d60e0768ca44a3097c4a51b8751f5a773904a00c
Petr Broz <ipetrbroz@gmail.com>
2020-03-12 13:30:48 +0100

    Addad simple svf-to-gltf test to run in Travis.

---
+++ test/remote-svf-to-gltf.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Basic SVF-to-glTF conversion test.
+# Usage:
+#   export FORGE_CLIENT_ID=<your client id>
+#   export FORGE_CLIENT_SECRET=<your client secret>
+#   ./remote-svf-to-gltf.sh <your model urn> <output folder>
+
+# Convert SVF to glTF
+node ./bin/forge-convert.js $1 --output-folder $2/gltf --deduplicate --skip-unused-uvs --ignore-lines --ignore-points
+
+# Iterate over glTFs generated for all viewables (in <urn>/<guid> subfolders)
+for gltf in $(find $2/gltf -name "output.gltf"); do
+    guid_dir=$(dirname $gltf)
+    guid=$(basename $guid_dir)
+    urn_dir=$(dirname $guid_dir)
+    urn=$(basename $urn_dir)
+    echo "Validating URN: $urn GUID: $guid"
+    node ./tools/validate.js $gltf
+done

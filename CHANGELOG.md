eeead81e510f5e56c1c43f18b3efb17ba5f2bd72
Petr Broz <ipetrbroz@gmail.com>
2020-03-13 13:57:09 +0100

    Added F2D downloader.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+- Added
+  - F2D downloader
+
 ## [3.2.0] - 2020-03-13
 
 - Added
---
+++ src/f2d/downloader.ts
@@ -0,0 +1,70 @@
+import * as path from 'path';
+import * as zlib from 'zlib';
+import * as fse from 'fs-extra';
+import { ModelDerivativeClient, ManifestHelper, IDerivativeResourceChild } from 'forge-server-utils';
+import { IAuthOptions } from 'forge-server-utils/dist/common';
+
+export interface IDownloadOptions {
+    outputDir?: string;
+    log?: (message: string) => void;
+}
+
+export interface IDownloadTask {
+    ready: Promise<void>;
+    cancel: () => void;
+}
+
+interface IDownloadContext {
+    log: (message: string) => void;
+    outputDir: string;
+    cancelled: boolean;
+}
+
+export class Downloader {
+    protected modelDerivativeClient: ModelDerivativeClient;
+
+    constructor(protected auth: IAuthOptions) {
+        this.modelDerivativeClient = new ModelDerivativeClient(this.auth);
+    }
+
+    download(urn: string, options?: IDownloadOptions): IDownloadTask {
+        const context: IDownloadContext = {
+            log: options?.log || ((message: string) => {}),
+            outputDir: options?.outputDir || '.',
+            cancelled: false
+        };
+        return {
+            ready: this._download(urn, context),
+            cancel: () => { context.cancelled = true; }
+        };
+    }
+
+    private async _download(urn: string, context: IDownloadContext): Promise<void> {
+        context.log(`Downloading derivative ${urn}`);
+        const helper = new ManifestHelper(await this.modelDerivativeClient.getManifest(urn));
+        const derivatives = helper.search({ type: 'resource', role: 'graphics' }) as IDerivativeResourceChild[];
+        const urnDir = path.join(context.outputDir, urn);
+        for (const derivative of derivatives.filter(d => d.mime === 'application/autodesk-f2d')) {
+            if (context.cancelled) {
+                return;
+            }
+            const guid = derivative.guid;
+            context.log(`Downloading viewable ${guid}`);
+            const guidDir = path.join(urnDir, guid);
+            fse.ensureDirSync(guidDir);
+            const baseUrn = derivative.urn.substr(0, derivative.urn.lastIndexOf('/'));
+            const manifestGzip = await this.modelDerivativeClient.getDerivative(urn, baseUrn + '/manifest.json.gz');
+            fse.writeFileSync(path.join(guidDir, 'manifest.json.gz'), manifestGzip);
+            const manifestGunzip = zlib.gunzipSync(manifestGzip);
+            const manifest = JSON.parse(manifestGunzip.toString());
+            for (const asset of manifest.assets) {
+                if (context.cancelled) {
+                    return;
+                }
+                context.log(`Downloading asset ${asset.URI}`);
+                const assetData = await this.modelDerivativeClient.getDerivative(urn, baseUrn + '/' + asset.URI);
+                fse.writeFileSync(path.join(guidDir, asset.URI), assetData);
+            }
+        }
+    }
+}

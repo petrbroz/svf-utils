90a191b35a29a728510525bf263b081f20de9ce6
Petr Broz <petr.broz@autodesk.com>
2022-03-24 09:44:21 +0100

    Removed OTG.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 export { Reader as SvfReader } from './svf/reader';
 export { Downloader as SvfDownloader } from './svf/downloader';
 export { Downloader as F2dDownloader } from './f2d/downloader';
-export { Reader as OtgReader } from './otg/reader';
-export { Downloader as OtgDownloader } from './otg/downloader';
 export { Writer as GltfWriter } from './gltf/writer';
 export { Region } from 'forge-server-utils/dist/common';

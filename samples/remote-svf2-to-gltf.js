d0812a2756f686357758d0086cdbfccbd5cbf738
Petr Broz <petr.broz@autodesk.com>
2024-12-18 15:25:13 +0100

    Updated SVF2 sample to process all available views.

--- samples/remote-svf2-to-gltf.js
+++ samples/remote-svf2-to-gltf.js
@@ -7,8 +7,8 @@ const [,, urn, outputDir] = process.argv;
 async function run() {
     const reader = await SVF2Reader.FromDerivativeService(urn, APS_ACCESS_TOKEN);
     const views = await reader.listViews();
-    if (views.length > 0) {
-        const view = views[0]; // Read the first view
+    for (const view of views) {
+        console.log('Processing view:', view.id);
         const scene = await reader.readView(view);
         const writer = new GltfWriter({
             deduplicate: false,
@@ -17,8 +17,6 @@ async function run() {
             log: console.log
         });
         await writer.write(scene, path.join(outputDir, view.id));
-    } else {
-        console.error('No views found in the SVF2 file.');
     }
 }
 
